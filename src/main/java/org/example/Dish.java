f94a3c46a1e9966212324965e58061e471c13202
Pushkar Kumar <103756675+beasthunter30@users.noreply.github.com>
2023-06-20 11:34:18 +0530

    createentity

--- src/main/java/org/example/Dish.java
+++ src/main/java/org/example/Dish.java
@@ -1,29 +1,56 @@
 package org.example;
 
-public class Dish {
-    String dish_name;
-    String type;
-    String price;
-    String id;
+public class Dish
+{
+    String id; //@ID
 
-    Dish(String id,String dish_name,String price,String type){
-        this.id = id;
-        this.dish_name = dish_name;
-        this.type = type;
+    String restroId;
+    String name;
+    //String description;
+    float price;
+
+    public void setRestroId(String restroId) {
+        this.restroId = restroId;
+    }
+
+    public void setPrice(float price) {
         this.price = price;
     }
 
+    public String getRestroId() {
+        return restroId;
+    }
 
-    public String getid(){
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getId()
+    {
         return id;
+    }
+
+
+    public float getPrice() {
+        return  price;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
     public String toString() {
         return "Dish{" +
-                "dishName='" + dish_name + '\'' +
-                ", restroId=" + id +
-                ", dishPrice=" + price +
+                "id='" + id + '\'' +
+                ", restroId='" + restroId + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
--- src/main/java/org/example/Restro.java
+++ src/main/java/org/example/Restro.java
@@ -1,13 +1,64 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
 
-public class Restro {
-    String restro_name;
-    String type;
+public class Restro
+{
     String id;
+    String name;
+    Location location;
+    List<Dish> menu;
+    Review[] reviews;
 
-    Restro(String id,String restro_name,String type){
+    public void setId(String id) {
         this.id = id;
-        this.restro_name = restro_name;        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setMenu(List<Dish> menu) {
+        this.menu = menu;
+    }
+
+    public void setReviews(Review[] reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public List<Dish> getMenu() {
+        return menu;
+    }
+
+    public Review[] getReviews() {
+        return reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "Restro{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location=" + location +
+                ", menu=" + menu +
+                ", reviews=" + Arrays.toString(reviews) +
+                '}';
     }
 }
